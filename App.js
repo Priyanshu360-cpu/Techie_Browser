ffaceb22325bdf7cb5a885a07c46c21cda0d5f44
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-10-04 22:09:37 +0530

    new commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+let uril="https://www.google.com";
+let a="";
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<>
+<View style={styles.container}>
+<View style={styles.marker}>
+<TextInput onChangeText={(text) => {a=text;console.log(a)}}
+ numberOfLines={10}
+ style={{ height:200, textAlignVertical: 'top',}}>
+ {"  "+uril}
+</TextInput>
+</View>
+</View>
+  <WebView
+        source={{ uri: uril}}
+      />
+</>
+     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#676d75',
+    height: 80,
   },
+  marker:{
+    position:'relative',
+    top:40,
+    backgroundColor:"#ffffff",
+    height:30
+  }
 });
