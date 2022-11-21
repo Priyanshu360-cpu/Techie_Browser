bc3866bee83dad941d2c08917cde456424bb98d6
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-11-21 20:06:42 +0530

    updated

--- incognito.js
+++ incognito.js
@@ -6,8 +6,12 @@ import roboincognito from './assets/roboincognito.png';
 export default function App({navigation, route }){
     return (
     <>
-    <View style={{width: 455, height: 1000,backgroundColor:"#302e2e"}} >
+    <View style={{width: 455, height: 1000,backgroundColor:"#302e2e"}}>
+    <View style={{position:'relative',top:50,left:50,backgroundColor:"white",height:28,width:255}}>
+        <TextInput onChangeText={(text)=>{console.log(text)}}></TextInput>
+    </View>
     <Image source={roboincognito} style={{height:200,width:200,position:'relative',top:280,left:85}}></Image>
     <Text style={{position:'relative',top:290,left:138,color:"white"}}>Incognito Mode</Text>
     </View>
-    </>);}
+    </>);
+    }
